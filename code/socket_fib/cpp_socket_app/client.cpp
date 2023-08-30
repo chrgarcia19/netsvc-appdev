95e0e2f31bae503d497fe4897fae9cf1d78e08c0
chrgarcia19 <christian.garcia@valpo.edu>
2023-08-30 08:47:26 -0500

    got the client and server transmitting a message

--- code/socket_fib/cpp_socket_app/client.cpp
+++ code/socket_fib/cpp_socket_app/client.cpp
@@ -3,8 +3,30 @@
 #include <iostream>
 #include <unistd.h>
 #include "fibonacci.h"
+#include <cstring>
+
+#define PORTNUM 4203
 
 int main(){
-    printf("%d" , fibonacci(5));
+    int socket_c, socket_s;
+    sockaddr_in server_addr;
+
+    socket_c = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORTNUM);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    connect(socket_c, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    char message[1024] = "Hello, Server!";
+    send(socket_c, message, strlen(message), 0);
+
+    char buffer[1024] = {0};
+    read(socket_c, buffer, sizeof(buffer));
+    std::cout << "Server replied: " << buffer << std::endl;
+
+    close(socket_c);
+
     return 0;
 }
--- code/socket_fib/cpp_socket_app/server.cpp
+++ code/socket_fib/cpp_socket_app/server.cpp
@@ -3,8 +3,38 @@
 #include <iostream>
 #include <unistd.h>
 #include "fibonacci.h"
+#include <cstring>
+
+#define PORTNUM 4203
 
 int main(){
-    printf("%d" , fibonacci(10));
+    int socket_s, socket_c;
+    sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+
+    socket_s = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORTNUM);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(socket_s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(socket_s, 5);
+
+    std::cout << "Server is listening on port 4203" << std::endl;
+
+    client_len = sizeof(client_addr);
+    socket_c = accept(socket_s, (struct sockaddr*)&client_addr, &client_len);
+
+    char buffer[1024] = {0};
+    read(socket_c, buffer, sizeof(buffer));
+
+    std::cout << "Received: " << buffer << std::endl;
+
+    send(socket_c, buffer, strlen(buffer), 0);
+
+    close(socket_c);
+    close(socket_s);
+
     return 0;
 }
