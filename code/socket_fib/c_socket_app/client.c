5664e3a0be8eb964d2dc1a363ca8bc772743a0b5
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-03 10:42:48 -0500

    Nuked client and server temporarily

--- code/socket_fib/c_socket_app/client.c
+++ code/socket_fib/c_socket_app/client.c
@@ -1,50 +1,15 @@
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdbool.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/resource.h>
 
-#define TESTING 	false
-#define PIPE_NAME 	"FIBOPIPE"
+#define TESTING		1
 
-int main(int argc, char **argv)
-	{
+int main(int argc, char **argv){
 	if(argc != 1){
 		fprintf(stderr, "[ERROR] Number of arguments of invalid!\n");
 		exit(EXIT_FAILURE);
 	}
-
-	///open pipe for writing 
-	int pipe = open(argv[0], O_WRONLY);
-	
-	if(pipe == -1){
-		perror("[ERROR] opening pipe");
-		exit(EXIT_FAILURE);
-	}
-
-	long fib = 1;	//input fib number from the user
-
-	///prompt and read integer from input
-	while(fib != 0){
-		printf("[QUERY] Enter a fibonacci number index: ");
-		if(fscanf(stdin, "%ld", &fib) == EOF){
-			perror("\n[ERROR] Attempting to fscanf failed");
-			exit(EXIT_FAILURE);
-		}
-		
-		///write int into FIFO while its not 0
-		if(write(pipe, &fib, sizeof(long)) == -1){
-			perror("[ERROR] Attempting to write to pipe failed");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	///close FIFO
-	close(pipe);
+	printf("Hello world!\n");
 
 	return(EXIT_SUCCESS);
 }
