f287bdafa673a9d0ba5d3837a5f4df5de6576d1a
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-05 11:55:48 -0500

    Accepting and closing sockets on server

--- code/socket_fib/c_socket_app/server.c
+++ code/socket_fib/c_socket_app/server.c
@@ -13,17 +13,17 @@ int equal_to    (int a, int b){ return a == b; }
 int less_than   (int a, int b){ return a < b;  }
 int not_equal_to(int a, int b){ return a != b; }
 // takes in error message to print if failed and received and expected return values as well as comparison to perform on the two
-void func_err_check(const char* ERR_MESSAGE, int ret_val, int (*compare)(int, int), int expected_val){
+void func_err_check(const char* ERR_MSG, int ret_val, int (*compare)(int, int), int expected_val){
 	if(compare(ret_val, expected_val)){
-		fprintf(stderr, "[ERROR] %s\n", ERR_MESSAGE);
+		fprintf(stderr, "[ERROR] %s\n", ERR_MSG);
 		if(DEBUG_STATEMENTS) printf("[DEBUG] Error code: %d\n", ret_val);
 		exit(EXIT_FAILURE);
 	}
 }
 
 int main(int argc, char **argv){
-	int port, err_codes, connected;
-	// reading in cl args
+	int port;
+	// reading in command line args
 	if(argc == 1){
 		port = START_PORT_RANGE;
 	}
@@ -41,36 +41,69 @@ int main(int argc, char **argv){
 		fprintf(stderr, "\nUsage: %s <port number>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	// performing socket magic
-	int sock_listen;
-	struct sockaddr_in my_addr;
+	// declare rest of variables now that we know the program didn't die
+	int err_code, is_connected;
+	int server_socket, client_socket;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_addr_len;
+	// creating socket
 	func_err_check(
 		"Creating the socket failed!", 
-		(sock_listen = socket(AF_INET, SOCK_STREAM, 0)),
+		(server_socket = socket(AF_INET, SOCK_STREAM, 0)),
 		&less_than,
 		0
 	);
+	// zero out the server_addr struct
+	bzero(&server_addr, sizeof(server_addr));
 
-	bzero(&my_addr, sizeof(my_addr));
-
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(port);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);	//allows server to accept a client on any interface
-
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);	//allows server to accept a client on any interface
+	// binding the socket
 	func_err_check(
 		"Binding the socket failed!",
-		err_codes = bind(
-			sock_listen, 
-			(struct sockaddr *) &my_addr, 
-			sizeof(my_addr)),
+		err_code = bind(
+			server_socket, 
+			(struct sockaddr *) &server_addr, 
+			sizeof(server_addr)
+		),
 		&less_than,
 		0
 	);
-	//waits for the client to connect
-	connected = 0;
-	err_codes = (sock_listen, 1);
+	// listening on the socket for a connection
+	func_err_check(
+		"Listening on the socket failed!",
+		listen(server_socket, 1),	// listen for only a single connection
+		&less_than,
+		0
+	);
 
+	func_err_check(
+		"Accepting the socket failed!",
+		err_code = accept(
+			server_socket,
+			(struct sockaddr*)&client_addr,
+			&client_addr_len
+		),
+		&less_than,
+		0
+	);
 	
+	// Doing the fibonacci stuff here
+
+	func_err_check(
+		"Closing the server socket failed!",
+		err_code = close(server_socket),
+		&less_than,
+		0
+	);
+
+	func_err_check(
+		"Closing the client socket failed!",
+		err_code = close(client_socket),
+		&less_than,
+		0
+	);
 
 	return(EXIT_SUCCESS);
 }
