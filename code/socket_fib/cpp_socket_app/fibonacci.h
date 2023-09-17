d1667d48c7c86e263ec45b0a3b96edc96c25749d
chrgarcia19 <christian.garcia@valpo.edu>
2023-09-17 14:25:30 -0500

    Fixed the n=0 case in fibonacci.h

--- code/socket_fib/cpp_socket_app/fibonacci.h
+++ code/socket_fib/cpp_socket_app/fibonacci.h
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int fibonacci(int n) {
-  if (n == 0 || n == 1) {
+  if (n == 0){
+    return 0;
+  }
+  if (n == 1) {
     return 1;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
