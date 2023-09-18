d8798cfe2da92f6aa60bfd6e51473fd35c0e5f9a
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-18 13:43:53 -0500

    Changed how fibonacci is indexed and added error check

--- code/socket_fib/c_socket_app/fibonacci.h
+++ code/socket_fib/c_socket_app/fibonacci.h
@@ -9,23 +9,27 @@ unsigned long fibonacci(unsigned long index);
 #define FIBONACCI_SRC
 #include <stdlib.h>
 // returns the fibonacci number at `index`
-// index must be greater than or equal to 0
+// index must be greater than 0
 unsigned long fibonacci(unsigned long index) {
-  unsigned long fib0 = 1;
   unsigned long fib1 = 1;
-  unsigned long fib2 = fib0 + fib1;
+  unsigned long fib2 = 1;
+  unsigned long fib3 = fib1 + fib2;
 
-  if (index == 0 || index == 1) {
-    return (index == 0 ? fib0 : fib1);
+  if (index == 0){
+    printf("[ERROR] Fibonacci index must be greater than 0\n");
+    exit(1);
+  }
+  else if (index == 1 || index == 2) {
+    return (index == 1 ? fib1 : fib2);
   }
 
   for (unsigned long i = 2; i < index; i++) {
-    fib0 = fib1;
     fib1 = fib2;
-    fib2 = fib0 + fib1;
+    fib2 = fib3;
+    fib3 = fib1 + fib2;
   }
 
-  return fib2;
+  return fib3;
 }
 
 #endif
