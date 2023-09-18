341d1471e146a74805e2aa275ebe134f56091682
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-18 15:57:17 -0500

    Should have made a soft link to fibonacci.h in C folder in the CPP folder

--- code/socket_fib/cpp_socket_app/fibonacci.h
+++ code/socket_fib/cpp_socket_app/fibonacci.h
@@ -1,9 +1,35 @@
-#include <iostream>
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
 
-int fibonacci(int n) {
-  if (n == 1 || n == 2) {
-    return 1;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
+unsigned long fibonacci(unsigned long index);
+
+#endif
+
+#ifndef FIBONACCI_SRC
+#define FIBONACCI_SRC
+#include <stdlib.h>
+// returns the fibonacci number at `index`
+// index must be greater than 0
+unsigned long fibonacci(unsigned long index) {
+  unsigned long fib1 = 1;
+  unsigned long fib2 = 1;
+  unsigned long fib3 = fib1 + fib2;
+
+  if (index == 0){
+    printf("[ERROR] Fibonacci index must be greater than 0\n");
+    exit(1);
   }
+  else if (index == 1 || index == 2) {
+    return (index == 1 ? fib1 : fib2);
+  }
+
+  for (unsigned long i = 2; i < index; i++) {
+    fib1 = fib2;
+    fib2 = fib3;
+    fib3 = fib1 + fib2;
+  }
+
+  return fib3;
 }
+
+#endif
