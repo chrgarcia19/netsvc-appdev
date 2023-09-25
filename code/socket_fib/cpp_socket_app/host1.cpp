f37bdddca33bd15e63f56f444652ddaf079be98a
chrgarcia19 <christian.garcia@valpo.edu>
2023-09-25 08:15:21 -0500

    added functionality for both files to start the socket connection and renamed them. Also updated gitignore

--- code/socket_fib/cpp_socket_app/host1.cpp
+++ code/socket_fib/cpp_socket_app/host1.cpp
@@ -6,16 +6,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  int port;
-  char *ip;
-  if (argc == 1){
-    ip = NULL;
-    port = 0;
-  }else if (argc == 3){
-    ip = argv[1];
-    port = atoi(argv[2]);
-  }
+void start_host(char* ip, int port){
   int server_socket, client_socket, fib_input;
   struct sockaddr_in server_addr;
   struct sockaddr_in client_addr;
@@ -51,8 +42,59 @@ int main(int argc, char *argv[]) {
   }
   
   close_two_sockets(&client_socket, &server_socket);
-
-  return 0;
-
 }
 
+void receive_host(char* ip, int port){
+  int client_socket, fib_input;
+  struct sockaddr_in server_addr;
+
+  srand((unsigned) time(NULL));
+
+  fib_input = 1;
+
+  client_socket = create_socket();
+
+  socket_setup(&server_addr, ip, port);
+
+  connect_socket(&client_socket, server_addr);
+
+  for (int i = 0; i < 10; i++) {
+    // sending the input of the fibonacci function to the server
+    write_data(&client_socket, fib_input);
+
+    // receiving the input of the fibonacci function from the server
+    fib_input = read_data(&client_socket, fib_input);
+
+    std::cout << "Server replied with: fibonacci(" << fib_input
+              << ") = " << fibonacci(fib_input) << std::endl;
+
+    fib_input = rand() % 40;
+  }
+
+  close_one_socket(&client_socket);
+}
+
+int main(int argc, char *argv[]) {
+  int port, host_order;
+  char *ip;
+  if (argc == 1){
+    ip = NULL;
+    port = 0;
+    host_order = 1;
+  }else if (argc == 2){
+    ip = NULL;
+    port = 0; 
+    host_order = atoi(argv[1]);
+  }else if (argc == 4){
+    ip = argv[1];
+    port = atoi(argv[2]);
+    host_order = atoi(argv[3]);
+  }
+
+  if (host_order == 1){
+    start_host(ip, port);
+  }else if (host_order == 2){
+    receive_host(ip, port);
+  }
+  return 0; 
+}
