ec4cf32c0af7eb6a6cb00d9bda0f9e2a2dbdf4bc
chrgarcia19 <christian.garcia@valpo.edu>
2023-10-02 13:23:35 -0500

    Modified max fibonacci input and ran clang-format

--- code/socket_fib/cpp_socket_app/host.cpp
+++ code/socket_fib/cpp_socket_app/host.cpp
@@ -3,89 +3,86 @@
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
 
 using namespace std;
 
-static void *run(void *arg){
-    int *socket = (int*) arg;
-    int timer = (2 + rand() % 9) * 1000;
-    int fib_input = rand() % 50;
-    cout << "[SEND] Fibonacci index: " << fib_input << endl;
-    write_data(socket, fib_input);
-    fib_input = read_data(socket, fib_input);
-    cout << "[RECEIVE] Fibonacci(" << fib_input << ") = " << fibonacci(fib_input) << endl;
-    cout << "Resetting timer in: " << timer/1000 << " seconds.\n" << endl;
-    return 0;
+static void *run(void *arg) {
+  int *socket = (int *)arg;
+  int timer = (2 + rand() % 9) * 1000;
+  int fib_input = rand() % 30;
+  cout << "[SEND] Fibonacci index: " << fib_input << endl;
+  write_data(socket, fib_input);
+  fib_input = read_data(socket, fib_input);
+  cout << "[RECEIVE] Fibonacci(" << fib_input << ") = " << fibonacci(fib_input)
+       << endl;
+  cout << "Resetting timer in: " << timer / 1000 << " seconds.\n" << endl;
+  return 0;
 }
 
-int main(int argc, char* argv[]){
-    int port, host_order;
-    char *ip;
-    if (argc == 1){
-        ip = NULL;
-        port = 0;
-        host_order = 1;
-    }else if (argc == 2){
-        ip = NULL;
-        port = 0; 
-        host_order = atoi(argv[1]);
-    }else if (argc == 3){
-        ip = argv[1];
-        port = atoi(argv[2]);
-        host_order = 1;
-    }else if (argc == 4){
-        ip = argv[1];
-        port = atoi(argv[2]);
-        host_order = atoi(argv[3]);
-    }
+int main(int argc, char *argv[]) {
+  int port, host_order;
+  char *ip;
+  if (argc == 1) {
+    ip = NULL;
+    port = 0;
+    host_order = 1;
+  } else if (argc == 2) {
+    ip = NULL;
+    port = 0;
+    host_order = atoi(argv[1]);
+  } else if (argc == 3) {
+    ip = argv[1];
+    port = atoi(argv[2]);
+    host_order = 1;
+  } else if (argc == 4) {
+    ip = argv[1];
+    port = atoi(argv[2]);
+    host_order = atoi(argv[3]);
+  }
 
-    int socket_h1, socket_h2;
-    struct sockaddr_in h1_addr;
-    struct sockaddr_in h2_addr;
-    socklen_t h2_len;
-    pthread_t thread;
+  int socket_h1, socket_h2;
+  struct sockaddr_in h1_addr;
+  struct sockaddr_in h2_addr;
+  socklen_t h2_len;
+  pthread_t thread;
 
-    srand((unsigned) time(NULL));
+  srand((unsigned)time(NULL));
 
-    
-    if (host_order == 1){
-        socket_h1 = create_socket();
+  if (host_order == 1) {
+    socket_h1 = create_socket();
 
-        port = socket_setup(&h1_addr, ip, port);
+    port = socket_setup(&h1_addr, ip, port);
 
-        bind_socket(&socket_h1, h1_addr);
-        listen_to_socket(&socket_h1, 1);
+    bind_socket(&socket_h1, h1_addr);
+    listen_to_socket(&socket_h1, 1);
 
-        cout << "Host 1 is waiting for a connection on port " << port << endl;
-        cout << endl;
+    cout << "Host 1 is waiting for a connection on port " << port << endl;
+    cout << endl;
 
-        h2_len = sizeof(h2_addr);
-        socket_h2 = accept(socket_h1, (struct sockaddr*)&h2_addr, &h2_len);
+    h2_len = sizeof(h2_addr);
+    socket_h2 = accept(socket_h1, (struct sockaddr *)&h2_addr, &h2_len);
 
-    }else if (host_order == 2){
-        socket_h2 = create_socket();
+  } else if (host_order == 2) {
+    socket_h2 = create_socket();
 
-        socket_setup(&h1_addr, ip, port); 
+    socket_setup(&h1_addr, ip, port);
 
-        connect_socket(&socket_h2, h1_addr);
-    }
+    connect_socket(&socket_h2, h1_addr);
+  }
 
-    
-    for (int i = 0; i < 10; i++){
-        cout << "Sending Message: " << i+1 << "/10" << endl;
-        pthread_create(&thread, NULL, &run, &socket_h2);
-        pthread_join(thread, NULL);
-    }
-    cout << "The thread has ended!" << endl;
+  for (int i = 0; i < 10; i++) {
+    cout << "Sending Message: " << i + 1 << "/10" << endl;
+    pthread_create(&thread, NULL, &run, &socket_h2);
+    pthread_join(thread, NULL);
+  }
+  cout << "The thread has ended!" << endl;
 
-    close_two_sockets(&socket_h2, &socket_h1);
+  close_two_sockets(&socket_h2, &socket_h1);
 
-    cout << "The sockets have been closed!" << endl;
+  cout << "The sockets have been closed!" << endl;
 
-
-    
-    return 0;
+  return 0;
 }
--- code/socket_fib/cpp_socket_app/host_functions.h
+++ code/socket_fib/cpp_socket_app/host_functions.h
@@ -7,69 +7,67 @@
 
 #define DEFAULT_PORT 4200
 
-int create_socket(){
-    /*AF_INET determines the type of IP address
-    AF_INET is used for IPv4.
-    SOCK_STREAM determines the type of connection.
-    SOCK_STREAM is TCP*/
-    int new_socket = socket(AF_INET, SOCK_STREAM, 0);
-    return new_socket;
+int create_socket() {
+  /*AF_INET determines the type of IP address
+  AF_INET is used for IPv4.
+  SOCK_STREAM determines the type of connection.
+  SOCK_STREAM is TCP*/
+  int new_socket = socket(AF_INET, SOCK_STREAM, 0);
+  return new_socket;
 }
 
-int socket_setup(struct sockaddr_in *socket_addr, char* ip, int port){
-    socket_addr->sin_family = AF_INET;
-    if (port == 0){//sets to default port num 
-        // htons converts a byte order into a network byte order
-        socket_addr->sin_port = htons(DEFAULT_PORT);
-        port = DEFAULT_PORT;
-    }else{//sets to specified port num
-        socket_addr->sin_port = htons(port);
-    }
-    if (ip != NULL){ //connects to a specific IP address
-        socket_addr->sin_addr.s_addr = inet_addr(ip);
-    }else{//connects to any IP address
-        // INADDR_ANY binds to any interface (IP or localhost)
-        socket_addr->sin_addr.s_addr = htonl(INADDR_ANY);
-    }
-    return port;
+int socket_setup(struct sockaddr_in *socket_addr, char *ip, int port) {
+  socket_addr->sin_family = AF_INET;
+  if (port == 0) { // sets to default port num
+    // htons converts a byte order into a network byte order
+    socket_addr->sin_port = htons(DEFAULT_PORT);
+    port = DEFAULT_PORT;
+  } else { // sets to specified port num
+    socket_addr->sin_port = htons(port);
+  }
+  if (ip != NULL) { // connects to a specific IP address
+    socket_addr->sin_addr.s_addr = inet_addr(ip);
+  } else { // connects to any IP address
+    // INADDR_ANY binds to any interface (IP or localhost)
+    socket_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+  }
+  return port;
 }
 
-void bind_socket(int *socket, struct sockaddr_in ip_addr){
-    bind(*socket, (struct sockaddr*)&ip_addr, sizeof(ip_addr));
+void bind_socket(int *socket, struct sockaddr_in ip_addr) {
+  bind(*socket, (struct sockaddr *)&ip_addr, sizeof(ip_addr));
 }
 
-void listen_to_socket(int *socket, int num_connections){
-    listen(*socket, num_connections);
+void listen_to_socket(int *socket, int num_connections) {
+  listen(*socket, num_connections);
 }
 
-void connect_socket(int *socket, struct sockaddr_in ip_addr){
-    connect(*socket, (struct sockaddr*)&ip_addr, sizeof(ip_addr));
+void connect_socket(int *socket, struct sockaddr_in ip_addr) {
+  connect(*socket, (struct sockaddr *)&ip_addr, sizeof(ip_addr));
 }
 
-int read_data(int *socket, int data){
-    read(*socket, &data, sizeof(data));
-    int new_data = ntohl(data);
-    return new_data; 
+int read_data(int *socket, int data) {
+  read(*socket, &data, sizeof(data));
+  int new_data = ntohl(data);
+  return new_data;
 }
 
-void write_data(int *socket, int data){
-    int new_data = htonl(data);
-    write(*socket, &new_data, sizeof(new_data));
+void write_data(int *socket, int data) {
+  int new_data = htonl(data);
+  write(*socket, &new_data, sizeof(new_data));
 }
 
-void close_one_socket(int *socket){
-    close(*socket);
-}
+void close_one_socket(int *socket) { close(*socket); }
 
-void close_two_sockets(int *socket1, int *socket2){
-    close(*socket1);
-    close(*socket2);
+void close_two_sockets(int *socket1, int *socket2) {
+  close(*socket1);
+  close(*socket2);
 }
 
 int fibonacci(int n) {
-  if (n == 0){
+  if (n == 0) {
     return 0;
-  }else if (n == 1 || n == 2) {
+  } else if (n == 1 || n == 2) {
     return 1;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
