eeae5ec412e83f0695fd8dd41a81628eff437227
chrgarcia19 <christian.garcia@valpo.edu>
2023-10-11 08:19:26 -0500

    Modified C++ socket fibonacci code to display output that's nearly identical to the Java version

--- code/socket_fib/cpp_socket_app/host.cpp
+++ code/socket_fib/cpp_socket_app/host.cpp
@@ -3,90 +3,85 @@
 
 using namespace std;
 
-
-void run1(int socket){
-    int delay = (1 + rand() % 5) * 1000;
-    cout << "[Timer Thread 1] Setting timer in " << delay/1000 << " seconds." << endl;
-    int fib_input = rand() % 30;
-    cout << "[Timer Thread 1: SEND] Fibonacci input: " << fib_input << endl;
-    write_data(&socket, fib_input);
-    int fib_out = read_data(&socket, fib_out);
-    cout << "[Timer Thread 1: RECEIVE] Fibonacci result: " << fibonacci(fib_out) << endl;
+void run(int socket) {
+  int delay = (1 + rand() % 5) * 1000;
+  cout << "[Timer Thread] Setting timer in " << delay / 1000 << " seconds."
+       << endl;
+  int fib_input = rand() % 30;
+  cout << "[Timer Thread: SEND] Fibonacci input: " << fib_input << endl;
+  write_data(&socket, fib_input);
 }
 
-void run2(int socket){
-    int fib_out = read_data(&socket, fib_out);
-    cout << "[Timer Thread 2: RECEIVE] Fibonacci result: " << fibonacci(fib_out) << endl;
-    int fib_input = rand() % 30;
-    cout << "[Timer Thread 2: SEND] Fibonacci input: " << fib_input << endl;
-    write_data(&socket, fib_input);
-}
+int main(int argc, char *argv[]) {
+  int port, host_order;
+  char *ip;
+  if (argc == 1) {
+    ip = NULL;
+    port = 0;
+    host_order = 1;
+  } else if (argc == 2) {
+    ip = NULL;
+    port = 0;
+    host_order = atoi(argv[1]);
+  } else if (argc == 3) {
+    ip = argv[1];
+    port = atoi(argv[2]);
+    host_order = 1;
+  } else if (argc == 4) {
+    ip = argv[1];
+    port = atoi(argv[2]);
+    host_order = atoi(argv[3]);
+  }
 
+  srand((unsigned)time(NULL));
 
+  int socket, fib_numb;
+  struct sockaddr_in sock_addr;
+  socklen_t sock_len;
+  bool read_index = true;
 
-int main(int argc, char* argv[]){
-    int port, host_order;
-    char *ip;
-    if (argc == 1) {
-        ip = NULL;
-        port = 0;
-        host_order = 1;
-    } else if (argc == 2) {
-        ip = NULL;
-        port = 0;
-        host_order = atoi(argv[1]);
-    } else if (argc == 3) {
-        ip = argv[1];
-        port = atoi(argv[2]);
-        host_order = 1;
-    } else if (argc == 4) {
-        ip = argv[1];
-        port = atoi(argv[2]);
-        host_order = atoi(argv[3]);
+  if (host_order == 1) {
+    socket = create_socket();
+
+    port = socket_setup(&sock_addr, ip, port);
+
+    bind_socket(&socket, sock_addr);
+    listen_to_socket(&socket, 1);
+
+    cout << "Host 1 is waiting for a connection on port " << port << endl;
+
+    sock_len = sizeof(sock_addr);
+    socket = accept(socket, (struct sockaddr *)&sock_addr, &sock_len);
+  } else if (host_order == 2) {
+    socket = create_socket();
+
+    socket_setup(&sock_addr, ip, port);
+
+    connect_socket(&socket, sock_addr);
+  }
+
+  for (int i = 0; i < 10; i++) {
+    cout << endl;
+    cout << "Message: " << i + 1 << "/10" << endl;
+    if (host_order == 1) {
+      thread t1(run, socket);
+      t1.join();
+      read_index = false;
     }
+    fib_numb = read_data(&socket, fib_numb);
+    if (read_index) {
+      cout << "[Main Thread: RECEIVE] Fibonacci index: " << fib_numb << endl;
 
-    srand((unsigned)time(NULL));
+      fib_numb = fibonacci(fib_numb);
 
-    int socket;
-    struct sockaddr_in sock_addr;
-    socklen_t sock_len;
+      write_data(&socket, fib_numb);
 
-    if (host_order == 1){
-        socket = create_socket();
-
-        port = socket_setup(&sock_addr, ip, port);
-
-        bind_socket(&socket, sock_addr);
-        listen_to_socket(&socket, 1);
-
-        cout << "Host 1 is waiting for a connection on port " << port << endl;
-        cout << endl;
-
-        sock_len = sizeof(sock_addr);
-        socket = accept(socket, (struct sockaddr *)&sock_addr, &sock_len);
-    } else if (host_order == 2){
-        socket = create_socket();
-
-        socket_setup(&sock_addr, ip, port);
-
-        connect_socket(&socket, sock_addr);
+    } else {
+      cout << "[Timer Thread: RECEIVE] Fibonacci result: " << fib_numb << endl;
+      read_index = true;
     }
+  }
 
-    for (int i = 0; i < 10; i++){
-        if (host_order == 1){
-            cout << "Message: " << i+1 << "/10" << endl;
-            thread t1(run1, socket);
-            t1.join();
-            cout << endl;
-        } else if (host_order == 2){
-            cout << "Message: " << i+1 << "/10" << endl;
-            thread t2(run2, socket);
-            t2.join();
-            cout << endl;
-        }
-    }
-    
-    close_one_socket(&socket);
-    close_two_sockets(&socket, &socket);
-    return 0;
+  close_one_socket(&socket);
+  return 0;
 }
